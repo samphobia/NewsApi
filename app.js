e969571d162e663aa6697dbd0b10e01caeb0bd21
Samuel Osinubi <samobafemisayo@gmail.com>
2023-01-25 08:00:57 +0100

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const options = {
   definition: {
     openapi:  "3.0.0",
     info: {
-      title: "Denukan API",
+      title: "Denukan CoreBanking API Collections",
       version: "1.0.0",
       description: "DENUKAN SOFTWARE API v2.2"
     },
