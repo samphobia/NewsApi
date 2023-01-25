1a3d6bbdc85a77ae57e0310beb14dc079c6a9cd0
Samuel Osinubi <samobafemisayo@gmail.com>
2023-01-25 07:58:29 +0100

    creation of transaction microservice

--- routes/transactions.js
+++ routes/transactions.js
@@ -4,6 +4,44 @@ const router = express.Router();
 
 const transactionsController = require("../controllers/transactions")
 
+/**
+ * @swagger
+ * tags:
+ *  name: Transactions
+ *  description: Transactions microservices
+ */
+
+/**
+ * @swagger
+ * /transaction/transactionOut:
+ *  post:
+ *    summary: transfer Funds
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              userId:
+ *                type: string
+ *              transactionTo:
+ *                type: string
+ *              amount:
+ *                type: string
+ *    tags: [Transactions]
+ *    responses: 
+ *      200:
+ *        description: Transfer successful
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *      400:
+ *        description: Transfer Declined
+ * 
+ */
+
 router.post('/transactionOut', transactionsController.transactionOut)
 
 module.exports = router;
